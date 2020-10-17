e46f0cb7ede89811deb0a340a396c6d90316b85f
chrispine-och <chrisochieng150@gmail.com>
2020-10-17 19:20:56 +0300

    modified the form and added its js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ function myFunction() {
       x.style.display = "none";
     }
   }
+
+
+  function myDiv() {
+    var x = document.getElementById("myDIV1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+ 
