59a8ffcf122b2eb81e7a4999c81a7d1a1f67167d
chrispine-och <chrisochieng150@gmail.com>
2020-10-17 23:14:38 +0300

    added js for total price

--- js/script.js
+++ js/script.js
@@ -17,3 +17,17 @@ function myFunction() {
     }
   }
  
+  function totalPrice (){
+    var price = 0;
+    var selectedSize = document.getElementById("sizes")
+    var CostOfSize = selectedSize.options[selectedSize.selectedIndex].value;
+    var selectedCrust = document.getElementById("crusts")
+    var CostOfCrust = selectedCrust.options[selectedCrust.selectedIndex].value;
+    var selectedTopping = document.getElementById("toppings")
+    var CostOfToppings = selectedTopping.options[selectedCrust.selectedIndex].value;
+    var QuantityOfPizza = document.getElementById("numberOfPizzas").value;
+    price = parseInt(CostOfSize) + parseInt(CostOfCrust) + parseInt(CostOfToppings)
+    totalPrice = price * parseInt(QuantityOfPizza);
+    document.getElementById("total").value = totalPrice;
+    event.preventDefault();
+  };
