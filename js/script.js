ee8d10d63a6c91b3c74b21fe36121b4155fab2a8
chrispine-och <chrisochieng150@gmail.com>
2020-10-17 23:15:50 +0300

    added js for tptal price delivary oder

--- js/script.js
+++ js/script.js
@@ -31,3 +31,16 @@ function myFunction() {
     document.getElementById("total").value = totalPrice;
     event.preventDefault();
   };
+  function totalPrice1 (){
+    var price = 0;
+    var selectedSize = document.getElementById("sizes1")
+    var CostOfSize = selectedSize.options[selectedSize.selectedIndex].value;
+    var selectedCrust = document.getElementById("crusts1")
+    var CostOfCrust = selectedCrust.options[selectedCrust.selectedIndex].value;
+    var selectedTopping = document.getElementById("toppings1")
+    var CostOfToppings = selectedTopping.options[selectedCrust.selectedIndex].value;
+    var QuantityOfPizza = document.getElementById("numberOfPizzas1").value;
+    price = parseInt(CostOfSize) + parseInt(CostOfCrust) + parseInt(CostOfToppings)
+    totalPrice = price * parseInt(QuantityOfPizza);
+    document.getElementById("totalDelivery").value = totalPrice;
+    event.preventDefault();
